133d72f40dc816e3ee5f1398560622e0114e2612
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-07-15 11:05:14 -0600

    feat: ejercicios 1_13, 1_14, 1_15

---
+++ capitulo_1/ejercicio_1_13/histogram_horizontal.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define IN 1 
+#define OUT 0
+#define MAX_WORD_SIZE 20
+#define MAX_WORD_SIZE 20
+
+int main()
+{
+    int c, nc, state, i, j;
+    char word[MAX_WORD_SIZE];
+
+    state = OUT;
+    nc = 0;
+    while((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            state = OUT;
+            for(i = 0; i < nc; i++)
+                putchar(word[i]);
+            for(j = 0; j < 10 - i; j++)
+                putchar(' ');
+
+            while (nc)
+            {
+                putchar("#");
+                nc--;
+            }
+            putchar('\n');
+        }
+        else if (state == IN)
+        {
+            word[nc] = c;
+            nc++;
+        }
+        else if (state == OUT)
+        {
+            word[nc] = c;
+            nc++;
+            state = IN;
+        }
+    }
+}
+
+// ./a.out < test.txt
---
+++ capitulo_1/ejercicio_1_15/func_ftoc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float ftoc(int fahr);
+
+int main(void)
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    printf(" °F     °C\n");
+    while (fahr <= upper)
+    {
+        celsius = ftoc(fahr);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }   
+}
+
+float ftoc(int fahr)
+{
+    int celsius = (5.0/9.0) * (fahr - 32);
+    return celsius;
+}
